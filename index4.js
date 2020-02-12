bfa32b86a8dc1502904c24c88843d1e98924fc74
Hardik Patel <hpatel@wsgc.com>
2020-02-12 06:47:44 -0800

    Update index4.js

--- index4.js
+++ index4.js
@@ -1,6 +1,12 @@
 const vm = new Vue({});
 
-Vue.component('button-counter',{
+const oddNumbersFunction = array => {
+    return array.filter(number => {
+        return number % 2 != 0;
+    });
+};
+
+Vue.component('button-counter', {
     data: function () {
         return {
             count: 0,
@@ -17,6 +23,14 @@ Vue.component('todo-item', {
 const app = new Vue({
     el: '#app',
     data: {
+        numberSets: [
+            [1, 2, 3, 4, 5, 7],
+            [11, 12, 13, 14, 15, 17]],
+        numbersArray: [1, 2, 3, 4, 5, 7],
+        myObject: {
+            title: `Title`,
+            author: `Author`,
+        },
         groceryList: [
             { id: 0, text: 'Veggie' },
             { id: 1, text: 'Meat' },
@@ -32,9 +46,25 @@ const app = new Vue({
         isTrue: true,
     },
     computed: {
+        oddNumbersComputed: function () {
+            return oddNumbersFunction(this.numbersArray);
+        },
         reverseText: function () {
             return this.myText.split(``).reverse()
                 .join(``);
         },
     },
+    methods: {
+        oddNumbers: function (numbers) {
+            return oddNumbersFunction(numbers);
+        },
+    },
+});
+
+Vue.set(app.myObject, `Subject`, `Subject`);
+app.$set(app.myObject, `Duration`, `Duration`);
+
+app.myObject = Object.assign({}, app.myObject, {
+    newProperty1: `newProperty1Value`,
+    newProperty2: `newProperty2`,
 });
